969f00927123efb655a2d5c3d28f52f4f8456079
DangDev <54974645+DangDev@users.noreply.github.com>
2021-01-30 22:42:22 +0700

    Update winfree.bat

--- winfree.bat
+++ winfree.bat
@@ -6,23 +6,25 @@ REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v Dis
 REG ADD HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System\CredSSP\Parameters\ /v AllowEncryptionOracle /t REG_DWORD /d 2
 Reg Add "HKCU\SOFTWARE\Microsoft\Windows\DWM" /v ColorPrevalence /t REG_DWORD /d 1 /f
 Reg Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v ColorPrevalence /t REG_DWORD /d 1 /f
-certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/curl.exe
-certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/7z.dll
-certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/7z.exe
-certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/nircmd.exe
+certutil -urlcache -split -f https://raw.githubusercontent.com/DangDev/winfree/main/files/curl.exe
+certutil -urlcache -split -f https://raw.githubusercontent.com/DangDev/winfree/main/files/7z.dll
+certutil -urlcache -split -f https://raw.githubusercontent.com/DangDev/winfree/main/files/7z.exe
+certutil -urlcache -split -f https://raw.githubusercontent.com/DangDev/winfree/main/files/nircmd.exe
 curl -LJOk https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip
 7z x ngrok-stable-windows-amd64.zip
 
 
-cd C:\Users\Administrator\Desktop
-curl -LJOk https://raw.githubusercontent.com/ThuongHai/thuonghai/master/ChromeSetup.exe
-curl -LJOk https://raw.githubusercontent.com/ThuongHai/thuonghai/master/anti-close.bat
+cd %USERPROFILE%
+mkdir WinFree
+cd Winfree
+curl -LJOk https://raw.githubusercontent.com/DangDev/winfree/main/files/ChromeSetup.exe
+curl -LJOk https://raw.githubusercontent.com/DangDev/winfree/main/files/anti-close.bat
 start ChromeSetup.exe
 cls
 @echo off
 set /p id="Paste NGROK AUTHTOKEN (Ctrl+V): "
 ngrok authtoken %id%
-start ngrok tcp 3389
+start ngrok tcp 3389 -region=au
 start anti-close.bat
 net start audiosrv
 taskkill /f /im sqlservr.exe
