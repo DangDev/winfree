0669521f6f1e0f7d1b7986f1f13d1bd445b84c0d
DangDev <54974645+DangDev@users.noreply.github.com>
2021-01-30 23:05:33 +0700

    Update account.bat

--- files/account.bat
+++ files/account.bat
@@ -1,2 +1,14 @@
-net user TinCowBoy Dangvip2k5pro /add
-net localgroup administrators TinCowBoy /add
+ECHO Do you want to create new account?
+set /p check="Type Yes(Y) or No(N): "
+
+IF "%check%"=="Y" or "%check%"=="Yes" (
+  set /p user="Enter your new Username: "
+  set /p pass="Enter your new account password: "
+  net user %user% %pass% /add
+  net localgroup administrators $user% /add
+)
+
+cd %USERPROFILE%\Winfree
+certutil -urlcache -split -f https://raw.githubusercontent.com/DangDev/winfree/main/files/wallpaper.bmp
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %USERPROFILE%\Winfree\wallpaper.bmp /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
