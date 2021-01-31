56207b51a399fc0541673cc32cd7f38cd4609755
DangDev <54974645+DangDev@users.noreply.github.com>
2021-01-31 12:30:48 +0700

    Update account.bat

--- files/account.bat
+++ files/account.bat
@@ -6,7 +6,7 @@ net user %user% %pass% /add
 net localgroup administrators %user% /add
 
 cd %USERPROFILE%\Winfree
-certutil -urlcache -split -f https://raw.githubusercontent.com/DangDev/winfree/main/files/wallpaper.bmp
+certutil.exe -urlcache -split -f https://raw.githubusercontent.com/DangDev/winfree/main/files/wallpaper.bmp
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %USERPROFILE%\Winfree\wallpaper.bmp /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 EXIT
